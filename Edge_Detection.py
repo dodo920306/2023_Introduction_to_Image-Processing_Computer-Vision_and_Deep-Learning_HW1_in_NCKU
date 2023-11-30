ff308ce9fae05d3893355dd73c5699ef64ce38eb
dodo920306 <dodo920306@gmail.com>
2023-11-30 21:51:18 +0800

    Try to fix alll overflow problems while maintaining reasonable an- swers

--- Edge_Detection.py
+++ Edge_Detection.py
@@ -75,17 +75,18 @@ class MyWidget(QGroupBox):
             img = cv2.imread(self.filename1)
             img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
             img = cv2.GaussianBlur(img, (3, 3), 0)
-            zero_padding = numpy.zeros((img.shape[0] + 2, img.shape[1] + 2))
+            zero_padding = numpy.zeros((img.shape[0] + 2, img.shape[1] + 2)).astype(numpy.int32)
             zero_padding[1:-1, 1:-1] = img
+            res = numpy.zeros_like(img).astype(numpy.int32)
 
             for x in range(img.shape[0]):
                 for y in range(img.shape[1]):
                     res_x = numpy.sum(zero_padding[x : x + 3, y : y + 3] * sobel_x) ** 2
                     res_y = numpy.sum(zero_padding[x : x + 3, y : y + 3] * sobel_y) ** 2
-                    img[x, y] = (res_x + res_y) ** 0.5
+                    res[x, y] = (res_x + res_y) ** 0.5
 
-            img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
-            cv2.imshow('combination', img)
+            img = numpy.where(res > 255, 255, res).astype(numpy.uint8)
+            cv2.imshow('combination', cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX))
             _, img = cv2.threshold(img, 128, 255, cv2.THRESH_BINARY)
             cv2.imshow('threshold', img)
         except AttributeError as e:
@@ -100,17 +101,19 @@ class MyWidget(QGroupBox):
             zero_padding = numpy.zeros((img.shape[0] + 2, img.shape[1] + 2))
             zero_padding[1:-1, 1:-1] = img
             gradient_angle = numpy.zeros_like(img, dtype='uint16')
+            res = numpy.zeros_like(img).astype(numpy.int32)
 
             for x in range(img.shape[0]):
                 for y in range(img.shape[1]):
                     res_x = numpy.sum(zero_padding[x : x + 3, y : y + 3] * sobel_x)
                     res_y = numpy.sum(zero_padding[x : x + 3, y : y + 3] * sobel_y)
-                    img[x, y] = (res_x ** 2 + res_y ** 2) ** 0.5
+                    res[x, y] = (res_x ** 2 + res_y ** 2) ** 0.5
                     gradient_angle[x, y] = (numpy.degrees(numpy.arctan2(res_y, res_x)) + 360) % 360
 
             mask1 = ((gradient_angle >= 120) & (gradient_angle <= 180)).astype(numpy.uint8) * 255
             mask2 = ((gradient_angle >= 210) & (gradient_angle <= 330)).astype(numpy.uint8) * 255
 
+            img = numpy.where(res > 255, 255, res).astype(numpy.uint8)
             img = cv2.normalize(img, None, 0, 255, cv2.NORM_MINMAX)
 
             cv2.imshow('Result 1', cv2.bitwise_and(img, mask1))
